14e3c01779a163675e4e7383f78d25f565e570d8
jason <jayyang1024@gmail.com>
2025-12-15 01:39:29 +0800

    add proxy template

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-my_lable="ubuntu_2204"
-vps_ip=$(vultr-cli instance list | grep $my_lable | awk '{print $2}')
-while [ -z "$vps_ip" -o "$vps_ip" == "0.0.0.0" ];do	
-    echo "vps $vps_ip is not exist"
-    sleep 1
-    vps_ip=$(vultr-cli instance list | grep $my_lable | awk '{print $2}')
-    vps_ip="0.0.0.1"
-done
