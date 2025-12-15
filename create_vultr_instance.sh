413ae586e0600be7d8f86ac1c6d0396232be63e5
jason <jayyang1024@gmail.com>
2025-12-15 16:28:51 +0800

    add proxy template

--- create_vultr_instance.sh
+++ create_vultr_instance.sh
@@ -8,12 +8,13 @@ my_lable="ubuntu_2204"
 my_tag="v2ray"
 my_ssh_keys="c5e8bf26-ab13-454a-a827-c2afff006a67,fa784b8e-c8d9-40d3-ab66-c7b0177a4013"
 vps_ip=$(vultr-cli instance list | grep $my_lable | awk '{print $2}')
+script_id="89005eb6-6e67-40fb-b873-c8399295f05e"
 
 function install_v2ray()
 {
     echo "begin to install v2ray"
     ssh -T -o StrictHostKeyChecking=no root@${vps_ip} 2>&1 << eof
-    bash <(curl -L https://raw.githubusercontent.com/JayYang1991/fhs-install-v2ray/master/install-release.sh)
+    bash <(curl -L https://raw.githubusercontent.com/JayYang1991/fhs-install-v2ray/master/install-v2ray-proxy-server.sh)
     exit
 eof
     echo "install v2ray $vps_ip success"
@@ -48,7 +49,7 @@ function check_ssh_until_success() {
     return 1
 }
 
-function undate_local_v2ray_agent_config() {
+function update_local_v2ray_agent_config() {
     local bash_config_file="~/.bashrc"
     local v2ray_config_file="/usr/local/etc/v2ray/config.json"
     local origin_v2ray_config_file="/tmp/proxy_client_config.json"
@@ -74,11 +75,11 @@ function undate_local_v2ray_agent_config() {
 if [ ! -z "$vps_ip" ];then
     echo "vps $vps_ip is exist"
     install_v2ray
-    undate_local_v2ray_agent_config
+    update_local_v2ray_agent_config
     exit 0
 fi
 echo "begin to create instance"
-vultr-cli instance create --region=$my_region --plan=$my_plan --os=$my_os --host=$my_host --label=$my_lable --tags=$my_tag --ssh-keys=$my_ssh_keys --ipv6
+vultr-cli instance create --region=$my_region --plan=$my_plan --os=$my_os --script-id=$script_id --host=$my_host --label=$my_lable --tags=$my_tag --ssh-keys=$my_ssh_keys --ipv6
 if [ $? -ne 0 ];then
     echo "create instance failed."
     exit 1;
@@ -95,7 +96,7 @@ check_ssh_until_success $vps_ip
 
 install_v2ray
 
-undate_local_v2ray_agent_config
+update_local_v2ray_agent_config
 
 
 
